b7c97e2e6414b12635affdca93b173c8be4b0a7f
Anas <anasnew99@gmail.com>
2021-10-22 15:13:18 +0530

    Making default export follow greenlock-express pattern

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const GreenLockExpress = require("greenlock-express");
 
 
-module.exports = (app,defaults={greenlockDefaults: {},managerDefaults: {},storeDefaults:{}})=>{
-    GreenLockExpress.init({
-        manager: {
-            module: `${__dirname}/../greenlockManager`,
-
-            defaults:defaults.managerDefaults,
-            storeDefaults: defaults.storeDefaults
-        },
-        // configDir: './greenlock.d',
-        ...(defaults.greenlockDefaults || {}),
-    }).serve(app);
+module.exports =  {
+        init: (defaults={greenlockDefaults: {},managerDefaults: {},storeDefaults:{}})=>GreenLockExpress.init({
+            manager: {
+                module: `${__dirname}/../greenlockManager`,
+    
+                defaults:defaults.managerDefaults,
+                storeDefaults: defaults.storeDefaults
+            },
+            // configDir: './greenlock.d',
+            ...(defaults.greenlockDefaults || {}),
+        })
 }
+    
+
